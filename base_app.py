adccebfccdbb2e9b075d5c52c36408beb866d689
Fabian <105725955+Fabian-B-explore@users.noreply.github.com>
2022-06-27 12:13:55 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -29,7 +29,7 @@ import joblib,os
 import pandas as pd
 
 # Vectorizer
-news_vectorizer = open("resources/thevect.pkl","rb")
+news_vectorizer = open("resources/thevectnotfitted.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
