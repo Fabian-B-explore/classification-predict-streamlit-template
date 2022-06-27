85d2999470f5601c21ae21c1b569ab0e8d73eaea
Fabian <105725955+Fabian-B-explore@users.noreply.github.com>
2022-06-27 13:01:27 +0200

    starting from scratch

--- base_app.py
+++ base_app.py
@@ -29,7 +29,7 @@ import joblib,os
 import pandas as pd
 
 # Vectorizer
-news_vectorizer = open("resources/thevectnotfitted.pkl","rb")
+news_vectorizer = open("resources/thevectISfitted.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
@@ -39,35 +39,31 @@ raw = pd.read_csv("resources/train.csv")
 def main():
 	"""Tweet Classifier App with Streamlit """
 
-	#customized our menu here
-	st.set_page_config(page_title="Team CBB5 API",page_icon="random")
-
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
 	st.title("Tweet Classifer")
 	st.subheader("Climate change tweet classification")
-	st.caption("Please choose a model from the dropdown menu on the left")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Information","Multi Logistic Regression", "Decision Tree","Random Forest","Naive-Bayes","Support Vector(linear)","Support Vector(rbf)"]
-	selection = st.sidebar.selectbox("Please choose a model here", options)
+	options = ["Prediction", "Information"]
+	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
 		st.markdown("Some information here")
+
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
 	# Building out the predication page
-	if selection == "Multi Logistic Regression":
-
-		st.success("You have selected the Multi Logistic Regression model")
+	if selection == "Prediction":
+		st.info("Prediction with ML Models")
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter a tweet below",placeholder="example: RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
+		tweet_text = st.text_area("Enter Text","Type Here")
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
@@ -81,112 +77,7 @@ def main():
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
-			st.balloons()
-#################################################################
-	if selection == "Random Forest":
 
-		st.success("You have selected the Random Forest model")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter a tweet below",placeholder="example: RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
-
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/RandomForestClassifier(random_state=42).pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
-			st.balloons()
-#########################################################################
-	if selection == "Naive-Bayes":
-
-		st.success("You have selected the Naive Bayes model")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter a tweet below",
-								  placeholder="example: RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
-
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(
-				open(os.path.join("resources/MultinomialNB(alpha=0.1).pkl"), "rb"))
-			prediction = predictor.predict(vect_text)
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
-			st.balloons()
-#########################################################################
-
-	if selection == "Support Vector(linear)":
-
-		st.success("You have selected the Support Vector with kernel set to Linear model")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter a tweet below",placeholder="example: RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
-
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/SVC(kernel='linear').pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
-			st.balloons()
-#########################################################################
-	if selection == "Support Vector(rbf)":
-
-		st.success("You have selected the Support Vector with kernel set to rbf model")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter a tweet below",placeholder="example: RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
-
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/SVC().pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
-			st.balloons()
-#########################################################################
-
-	if selection == "Decision Tree":
-
-		st.success("You have selected the Decision Tree model")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter a tweet below",placeholder="example: RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
-
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/DecisionTreeClassifier(random_state=42).pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
-			st.balloons()
-
-# Required to let Streamlit instantiate our web app.  
+# Required to let Streamlit instantiate our web app.
 if __name__ == '__main__':
 	main()
