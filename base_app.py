e7d2ac8f63d51ff7a0bc585f41ad90007efa5c1f
William X <williammxh@ymail.com>
2022-06-30 05:17:00 +0200

    Added Landing Page

--- base_app.py
+++ base_app.py
@@ -29,8 +29,12 @@ def remove_punctuation(tweet):
 	tweet = tweet.rstrip()
 	tweet = tweet.replace('  ', ' ')
 	return tweet
-	
 
+# Get the Keys
+def get_key(val,my_dict):
+	for key,value in my_dict.items():
+		if val == value:
+			return key
 
 # Vectorizer
 news_vectorizer = open("resources/fittedVect.pkl","rb")
@@ -44,19 +48,53 @@ def main():
 	"""Tweet Classifier App with Streamlit """
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Global Tech Data Inc. Tweet Classifier")
-	header_image = Image.open('resources/ourLogo.png')
-	st.image(header_image, use_column_width='auto')
-	st.subheader("Determine Users Climate Change Belief by classifying their tweets")
+	jumbotron = Image.open('resources/jumbotron.jpeg')
+	st.image(jumbotron, use_column_width='auto')
+	st.subheader("Global Tech Data Inc. Tweet Classifier")
+	# header_image = Image.open('resources/ourLogo.png')
+	# st.image(header_image, use_column_width='auto')
+	# st.subheader("Determine Users Climate Change Belief by classifying their tweets")
 	t_choices = {2: "linking to factual news about climate change",
 			   1: "supporting the belief of man-made climate change",
 			   0: "neither supporting nor refuting the belief of man-made climate change",
 			   -1: "as not not believing in man-made climate change"}
 
+	# Prediction Labels
+	prediction_labels = {'Anti Climate Chabge': -1,
+						'Neutral about Climate Change': 0,
+						'Pro Climate Change': 1,
+						'News about Climatte Change': 2}
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Welcome","More Information","How to Use","Make a Prediction","About Us","Contact Us"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+	options = ["Welcome","More Information","How to Use","Try other Models","About Us","Contact Us"]
+	selection = st.sidebar.selectbox("Menu", options)
+#####################
+	# Building out the "Welcome" page
+	if selection == "Welcome":
+		# Welcome test for the landing page
+		st.write(
+		"""
+		Do you care about **Climate Change**? Here's a climate change tweet sentiment classifier.
+
+		Try it right Now! We'll predict your climate change sentiment based on your tweet.
+		Or it could simply be News! Try it out below.
+		"""
+		)
+		# Creating a text box for user input
+		welcome_tweet_text = st.text_area("Enter a tweet",
+								placeholder="eg. RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
+
+		links_removed = remove_links_and_usernames(welcome_tweet_text)
+		sw_removed = remove_stop_words(links_removed)
+		pun_removed = remove_punctuation(sw_removed)
+		vect_text = tweet_cv.transform([pun_removed]).toarray()
+
+		if st.button("Classify"):
+			predictor = joblib.load(open(os.path.join("resources/SVC().pkl"), "rb"))
+			prediction = predictor.predict(vect_text)
+			final_result = get_key(prediction,prediction_labels)
+			st.success("Tweet classified as {}".format(final_result))
+
 #####################
 	# Building out the "Information" page
 	if selection == "More Information":
@@ -77,12 +115,16 @@ def main():
 			st.markdown("Anti: the tweet does not believe in man-made climate change")
 #####################
 	# Building out the predication page
-	if selection == "Make a Prediction":
+	if selection == "Try other Models":
 		# Creating a text box for user input
 		st.subheader("Make a Prediction")
-		modelChoice = st.selectbox(on_change=None,label='Please choose a model',options=("Multi Logistic Regression","Decision Tree","Random Forest","Naive-Bayes","SVC(kernel=linear)","SVC(kernel=rbf)"))
+		modelChoice = st.selectbox(on_change=None,
+									label='Please choose a model',
+									options=("Multi Logistic Regression","Decision Tree","Random Forest","Naive-Bayes","SVC(kernel=linear)","SVC(kernel=rbf)"))
 		#modelChoice = st.radio("Choose a model", ("Multi Logistic Regression","Decision Tree","Random Forest","Naive-Bayes","SVC(kernel=linear)","SVC(kernel=rbf)"))
-		tweet_text = st.text_area("Enter a tweet below",placeholder="eg. RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
+		tweet_text = st.text_area("Enter a tweet",
+									placeholder="eg. RT @SoyNovioDeTodas: It's 2016, and a racist, sexist, climate change denying bigot is leading in the polls. #ElectionNight")
+
 
 		if modelChoice == "Multi Logistic Regression":
 			# Transforming user input with vectorizer
@@ -93,7 +135,8 @@ def main():
 			if st.button("Classify"):
 				predictor = joblib.load(open(os.path.join("resources/LogisticRegression().pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
-				st.success("Tweet classified as a {}".format(prediction))
+				final_result = get_key(prediction,prediction_labels)
+				st.success("Tweet classified as {}".format(final_result))
 
 
 		elif modelChoice == "Decision Tree":
@@ -104,7 +147,8 @@ def main():
 			if st.button("Classify"):
 				predictor = joblib.load(open(os.path.join("resources/DecisionTreeClassifier(random_state=42).pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
-				st.success("Tweet classified as a {}".format(prediction))
+				final_result = get_key(prediction,prediction_labels)
+				st.success("Tweet classified as {}".format(final_result))
 
 		elif modelChoice == "Random Forest":
 			links_removed = remove_links_and_usernames(tweet_text)
@@ -114,7 +158,8 @@ def main():
 			if st.button("Classify"):
 				predictor = joblib.load(open(os.path.join("resources/RandomForestClassifier(random_state=42).pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
-				st.success("Tweet classified as a {}".format(prediction))
+				final_result = get_key(prediction,prediction_labels)
+				st.success("Tweet classified as {}".format(final_result))
 
 		elif modelChoice == "Naive-Bayes":
 			links_removed = remove_links_and_usernames(tweet_text)
@@ -124,7 +169,8 @@ def main():
 			if st.button("Classify"):
 				predictor = joblib.load(open(os.path.join("resources/MultinomialNB(alpha=0.1).pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
-				st.success("Tweet classified as a {}".format(prediction))
+				final_result = get_key(prediction,prediction_labels)
+				st.success("Tweet classified as {}".format(final_result))
 
 		elif modelChoice == "SVC(kernel=linear)":
 			links_removed = remove_links_and_usernames(tweet_text)
@@ -134,7 +180,8 @@ def main():
 			if st.button("Classify"):
 				predictor = joblib.load(open(os.path.join("resources/SVC(kernel='linear').pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
-				st.success("Tweet classified as a {}".format(prediction))
+				final_result = get_key(prediction,prediction_labels)
+				st.success("Tweet classified as {}".format(final_result))
 
 		elif modelChoice == "SVC(kernel=rbf)":
 			links_removed = remove_links_and_usernames(tweet_text)
@@ -144,7 +191,8 @@ def main():
 			if st.button("Classify"):
 				predictor = joblib.load(open(os.path.join("resources/SVC().pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
-				st.success("Tweet classified as a {}".format(prediction))
+				final_result = get_key(prediction,prediction_labels)
+				st.success("Tweet classified as {}".format(final_result))
 
 ########################
 	if selection == "How to Use":
